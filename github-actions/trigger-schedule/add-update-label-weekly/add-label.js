d2d0d8e820d9720b37d6e254129a1091bd2a0cb9
Roslyn Wythe <roslynwythe@gmail.com>
2023-07-10 14:42:58 -0700

    Update add-label.js

--- github-actions/trigger-schedule/add-update-label-weekly/add-label.js
+++ github-actions/trigger-schedule/add-update-label-weekly/add-label.js
@@ -11,7 +11,7 @@ const updatedByDays = 3; // If there is an update within 3 days, the issue is co
 const inactiveUpdatedByDays = 14; // If no update within 14 days, the issue is considered '2 weeks inactive'
 const commentByDays = 7; // If there is an update within 14 days but no update within 7 days, the issue is considered outdated and the assignee needs 'To Update !' it
 const threeDayCutoffTime = new Date()
-threeDayCutoffTime.setDate(threeDayCutoffTime.getDate() - updatedByDays + 5)
+threeDayCutoffTime.setDate(threeDayCutoffTime.getDate() - updatedByDays + 3)
 const sevenDayCutoffTime = new Date()
 sevenDayCutoffTime.setDate(sevenDayCutoffTime.getDate() - commentByDays + 7)
 const fourteenDayCutoffTime = new Date()
