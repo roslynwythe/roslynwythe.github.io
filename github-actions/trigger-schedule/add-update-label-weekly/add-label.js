09554e56de5e592c47e0e40ff0805d92393eb2e2
Roslyn Wythe <roslynwythe@gmail.com>
2023-07-12 00:08:16 -0700

    Update add-label.js

--- github-actions/trigger-schedule/add-update-label-weekly/add-label.js
+++ github-actions/trigger-schedule/add-update-label-weekly/add-label.js
@@ -11,11 +11,11 @@ const updatedByDays = 3; // If there is an update within 3 days, the issue is co
 const inactiveUpdatedByDays = 14; // If no update within 14 days, the issue is considered '2 weeks inactive'
 const commentByDays = 7; // If there is an update within 14 days but no update within 7 days, the issue is considered outdated and the assignee needs 'To Update !' it
 const threeDayCutoffTime = new Date()
-threeDayCutoffTime.setDate(threeDayCutoffTime.getDate() - updatedByDays + 7)
+threeDayCutoffTime.setDate(threeDayCutoffTime.getDate() - updatedByDays + 3)
 const sevenDayCutoffTime = new Date()
-sevenDayCutoffTime.setDate(sevenDayCutoffTime.getDate() - commentByDays + 9)
+sevenDayCutoffTime.setDate(sevenDayCutoffTime.getDate() - commentByDays + 7)
 const fourteenDayCutoffTime = new Date()
-fourteenDayCutoffTime.setDate(fourteenDayCutoffTime.getDate() - inactiveUpdatedByDays + 14)
+fourteenDayCutoffTime.setDate(fourteenDayCutoffTime.getDate() - inactiveUpdatedByDays)
 
 /**
  * The main function, which retrieves issues from a specific column in a specific project, before examining the timeline of each issue for outdatedness.
